347bebfb578ad83ec3fc660dcbd7f093411f9c43
iharshit009 <harshitjain1309@gmail.com>
2019-12-07 02:25:59 +0530

    working with bootstrap and css in django templates

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,7 @@ def home(request):
     context = {
         'posts': posts
     }
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
