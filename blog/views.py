49767780548aa477f4d6c604cf570a3e646ae220
iharshit009 <harshitjain1309@gmail.com>
2019-12-07 21:18:49 +0530

    made a user and admin page and worked with database

--- blog/views.py
+++ blog/views.py
@@ -1,37 +1,15 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': "coreyMS",
-        'title': 'blog post 1',
-        'content': "First post content",
-        'date-posted': "august 24, 2018"
-    },
-    {
-        'author': "harhsit",
-        'title': 'blog post 2',
-        'content': "First2 post content",
-        'date-posted': "august 28, 2018"
-    },
-    {
-        'author': "arpit",
-        'title': 'blog post 1',
-        'content': "First post content",
-        'date-posted': "august 24, 2018"
-    }
-
-]
+from .models import Post
 
 # Create your views here.
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
