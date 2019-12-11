74ad8628d373f34acda4c809c2febe97e3ac06f8
iharshit009 <harshitjain1309@gmail.com>
2019-12-12 01:43:12 +0530

    completed the 9th cchap

--- django_learn/urls.py
+++ django_learn/urls.py
@@ -19,12 +19,13 @@ from users import views as user_views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('profile/', auth_views.LoginView.as_view(template_name='users/profile.html'), name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path("register/", user_views.register, name="register"),
 ]
