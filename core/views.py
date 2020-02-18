e1db99216a1b9edbfc2cd0bc96a32b16a34bd4a9
iharshit009 <harshitjain1309@gmail.com>
2020-02-18 15:02:31 +0530

    first completed

---
+++ core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'name': 'john',
+            'age': 23
+        }
+
+        return Response(data)
+
+
+# def test_view(request):
+#     data = {
+#         'name': 'john',
+#         'age' : 23
+#     }
+#     return JsonResponse(data)
